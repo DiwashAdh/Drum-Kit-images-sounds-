5085156af35f07fcb6e3d0f350e9119150d10e5e
Diwash Adh <76644083+DiwashAdh@users.noreply.github.com>
2023-06-16 18:57:38 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+var drums = document.querySelectorAll(".drum").length;
+
+for (var i = 0 ; i < drums ; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var button = this.innerHTML;
+        clicked(button);
+        animation(button);
+    });
+}
+
+document.addEventListener("keypress", function(event){
+    clicked(event.key);
+    animation(event.key);
+});
+
+
+
+//Play sound when key is pressed
+function clicked(key){
+    
+    switch (key) {
+        case "w":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();    
+            break;
+           
+        case "d":
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();          
+            break;
+    
+        case "j":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();              
+            break;
+        case "k":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();               
+            break;
+    
+        default:
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+    }
+}
+
+
+function animation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100)
+}
+
